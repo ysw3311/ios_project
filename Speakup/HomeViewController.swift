a9b9205313367a8d5df17c1d66c4d87361dffee3
ysw3311 <dktk0914@naver.com>
2026-06-08 06:32:56 +0900

    홈 화면 UI 추가 (타이틀, 연습 시작 버튼)

--- Speakup/HomeViewController.swift
+++ Speakup/HomeViewController.swift
@@ -2,8 +2,49 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "SpeakUp"
+        label.font = .boldSystemFont(ofSize: 36)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let startButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("오늘 연습하기", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "SpeakUp"
+        view.backgroundColor = .systemBackground
+        setupLayout()
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+    }
+
+    private func setupLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(startButton)
+
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -60),
+
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            startButton.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 32),
+            startButton.widthAnchor.constraint(equalToConstant: 200),
+            startButton.heightAnchor.constraint(equalToConstant: 52),
+        ])
+    }
+
+    @objc private func startButtonTapped() {
+        // TODO: ScriptViewController로 전환
     }
 }
